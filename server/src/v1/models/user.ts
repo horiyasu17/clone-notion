33c98afc27c689162ae1ebf3aa269e09ab14ff3f
horiyasu17 <horiyasu17@gmail.com>
2024-06-05 23:39:12 +0900

    create user model

---
+++ server/src/v1/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document } from "mongoose";
+
+export interface UserDoc extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model<UserDoc>("User", userSchema);
