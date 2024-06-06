3c35b5b76188bca800bdd98cb589b858d7616ea2
horiyasu17 <horiyasu17@gmail.com>
2024-06-06 16:14:50 +0900

    create AuthLayout.tsx

--- client/src/components/layouts/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout.tsx
@@ -1,3 +1,24 @@
+import { Box, Container } from "@mui/material";
+import notionLogo from "src/assets/images/notion-logo.png";
+
 export const AuthLayout = () => {
-  return <>AuthLayout</>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          mt: 6,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src={notionLogo}
+          alt="logo"
+          style={{ width: 100, height: 100, marginBottom: 3 }}
+        />
+        Notion Clone
+      </Box>
+    </Container>
+  );
 };
