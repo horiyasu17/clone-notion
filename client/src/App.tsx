b0d84f9efe8757ef89f36fbada36c3cd76c7f77c
horiyasu17 <horiyasu17@gmail.com>
2024-06-06 15:24:18 +0900

    create client base components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthLayout } from "./components/layouts/AuthLayout";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 function App() {
-  return <div className="App"></div>;
+  const theme = createTheme({
+    palette: { primary: blue },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
 
 export default App;
