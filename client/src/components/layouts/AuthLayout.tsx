7b02704f2fa54b2cec2344604d5340498f585451
horiyasu17 <horiyasu17@gmail.com>
2024-06-07 11:07:06 +0900

    create auth redirect

--- client/src/components/layouts/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout.tsx
@@ -1,8 +1,12 @@
 import { Box, Container } from "@mui/material";
 import notionLogo from "src/assets/images/notion-logo.png";
 import { Outlet } from "react-router-dom";
+import { useAuthorization } from "src/hooks/useAuthorization";
 
 export const AuthLayout = () => {
+  // check token
+  useAuthorization();
+
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -13,11 +17,7 @@ export const AuthLayout = () => {
           flexDirection: "column",
         }}
       >
-        <img
-          src={notionLogo}
-          alt="logo"
-          style={{ width: 100, height: 100, marginBottom: 3 }}
-        />
+        <img src={notionLogo} alt="logo" style={{ width: 100, height: 100, marginBottom: 3 }} />
         Notion Clone
       </Box>
       <Outlet />
