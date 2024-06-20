92f0121eb85d5be8aa4b5efd5afcfce94193ec59
horiyasu17 <horiyasu17@gmail.com>
2024-06-20 09:52:07 +0900

    repair create memo function

--- client/src/hooks/useSidebar.ts
+++ client/src/hooks/useSidebar.ts
@@ -2,12 +2,13 @@ import { useCallback, useEffect, useState } from 'react';
 import memoApi from 'src/api/memoApi';
 import { useDispatch } from 'react-redux';
 import { setAllMemoData } from 'src/redux/features/memoSlice';
-import { AppDispatch } from 'src/redux/store';
+import { AppDispatch, RootState, useSelector } from 'src/redux/store';
 import { AxiosError } from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export const useSidebar = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const allMemoData = useSelector((state: RootState) => state.memo.data);
   const { memoId } = useParams();
   const navigate = useNavigate();
   const [selectedMemoId, setSelectedMemoId] = useState<string | null>(null);
@@ -16,10 +17,12 @@ export const useSidebar = () => {
   const createMemo = useCallback(async () => {
     try {
       // create memo
-      await memoApi.create();
+      const { data } = await memoApi.create();
       // update sidebar memo title
-      const { data } = await memoApi.getAll();
-      dispatch(setAllMemoData(data));
+      const newAllMemo = [data, ...allMemoData];
+      dispatch(setAllMemoData(newAllMemo));
+      // redirect
+      navigate(`/memo/${data._id}`);
     } catch (error: unknown) {
       if (error instanceof AxiosError) alert(error.message);
     }
