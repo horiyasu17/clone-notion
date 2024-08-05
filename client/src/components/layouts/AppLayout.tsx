5bebc83d2b317d61b185cf1cdb80af2ac439014c
horiyasu17 <horiyasu17@gmail.com>
2024-08-06 06:41:52 +0900

    refactoring initialize

--- client/src/components/layouts/AppLayout.tsx
+++ client/src/components/layouts/AppLayout.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useAuthorization } from 'src/hooks/useAuthorization';
 import { Box } from '@mui/material';
 import { Sidebar } from 'src/components/common/Sidebar';
 import { Outlet } from 'react-router-dom';
 import { useInitialize } from 'src/hooks/useInitialize';
 
 export const AppLayout: React.FC = () => {
-  // check user login
-  useAuthorization();
   // initialize
   useInitialize();
 
